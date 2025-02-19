aca482075b71ce6c42e307cd45064a8644f70270
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-19 19:36:13 +0200

    finish theme cubit

--- lib/core/theme/themecubuit_cubit.dart
+++ lib/core/theme/themecubuit_cubit.dart
@@ -1,14 +1,24 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluxstore/core/database/cache/cache_helper.dart';
+import 'package:fluxstore/core/database/cache/cache_keys.dart';
+import 'package:fluxstore/core/service/service_locator.dart';
 
 part 'themecubuit_state.dart';
 
 class ThemecubuitCubit extends Cubit<ThemecubuitState> {
   ThemecubuitCubit() : super(ThemecubuitInitial());
-
-void changeTheme({bool? isDark}){
-  
-}
-  
+  bool isDark = true;
+  Future<void> changeTheme({bool? mode}) async {
+    if (mode != null) {
+      isDark = mode;
+      emit(ThemecubuitChanges(isDark: isDark));
+    } else {
+      isDark = !isDark;
+      getit.get<CacheHelper>().setBool(CacheKeys.savedTheme, isDark).then((
+        value,
+      ) {
+        emit(ThemecubuitChanges(isDark: isDark));
+      });
+    }
+  }
 }
--- lib/core/theme/themecubuit_state.dart
+++ lib/core/theme/themecubuit_state.dart
@@ -1,10 +1,11 @@
 part of 'themecubuit_cubit.dart';
 
-
 abstract class ThemecubuitState {}
 
 class ThemecubuitInitial extends ThemecubuitState {}
-class ThemecubuitDark extends ThemecubuitState {}
-class ThemecubuitLight extends ThemecubuitState {}
 
+class ThemecubuitChanges extends ThemecubuitState {
+  final bool isDark;
 
+  ThemecubuitChanges({required this.isDark});
+}

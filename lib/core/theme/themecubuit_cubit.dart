38f65c7059640ea0349ce86ea378dcb9c87d1f81
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-19 19:22:16 +0200

    create cache helper

---
+++ lib/core/theme/themecubuit_cubit.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'themecubuit_state.dart';
+
+class ThemecubuitCubit extends Cubit<ThemecubuitState> {
+  ThemecubuitCubit() : super(ThemecubuitInitial());
+
+void changeTheme({bool? isDark}){
+  
+}
+  
+}

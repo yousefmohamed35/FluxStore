7debce8219e06b9685c0c5f782db0a5aa5f1468c
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-19 18:40:01 +0200

    create styles class

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textStyle25 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w700,
+  );
+   static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const textStyle12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const textStyle17 = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+  );
+  static const textStyle10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+  );
+  static const textStyle22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+  );
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fluxstore/core/utils/app_router.dart';
 
 void main() {
   runApp(FluxStore());
 }
+
 class FluxStore extends StatelessWidget {
   const FluxStore({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
-    );
+    return MaterialApp.router(routerConfig: AppRouter.router);
   }
 }

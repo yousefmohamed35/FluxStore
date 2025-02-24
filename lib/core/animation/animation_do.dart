2956281f74c36c7d0bcd059ce8dc36bf8e0a7fbd
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-24 12:53:23 +0200

    create custom animation Fade In up

---
+++ lib/core/animation/animation_do.dart
@@ -0,0 +1,23 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+class CustomFadeInUp extends StatelessWidget {
+  const CustomFadeInUp({
+    required this.child,
+    required this.duration,
+    super.key,
+  });
+
+  final Widget child;
+
+  final int duration;
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInUp(
+      delay: const Duration(milliseconds: 300),
+      duration: Duration(milliseconds: duration),
+      child: child,
+    );
+  }
+}
--- lib/features/splash/presentation/view/widget/splash_view_item.dart
+++ lib/features/splash/presentation/view/widget/splash_view_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluxstore/core/animation/animation_do.dart';
 import 'package:fluxstore/core/utils/styles.dart';
 import 'package:fluxstore/core/widgets/custom_button.dart';
 
@@ -7,23 +8,26 @@ class SplashViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Text(
-          "Welcome to GemStore!",
-          style: Styles.textStyle25.copyWith(color: Colors.white),
-        ),
-        SizedBox(height: 20),
-        Text(
-          " The home for a fashionista",
-          style: Styles.textStyle12.copyWith(color: Colors.white),
-        ),
-        SizedBox(height: 60),
-        CustomButton(),
-        SizedBox(height: 100),
-      ],
+    return CustomFadeInUp(
+      duration: 600,
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            "Welcome to GemStore!",
+            style: Styles.textStyle25.copyWith(color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          Text(
+            " The home for a fashionista",
+            style: Styles.textStyle12.copyWith(color: Colors.white),
+          ),
+          SizedBox(height: 60),
+          CustomButton(),
+          SizedBox(height: 100),
+        ],
+      ),
     );
   }
 }
-

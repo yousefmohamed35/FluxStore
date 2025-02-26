86f05f1846f133808131c1cb30363afa213393d8
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-26 21:08:07 +0200

    update custom text button

--- lib/features/Auth/presentation/view/widgets/custom_text_button.dart
+++ lib/features/Auth/presentation/view/widgets/custom_text_button.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fluxstore/core/utils/app_router.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomTextButton extends StatelessWidget {
-  const CustomTextButton({
-    super.key,
-  });
+  const CustomTextButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      style: ButtonStyle(
-        padding: WidgetStatePropertyAll(EdgeInsets.zero),
-      ),
-      onPressed: () {},
+      style: ButtonStyle(padding: WidgetStatePropertyAll(EdgeInsets.zero)),
+      onPressed: () {
+        GoRouter.of(context).push(AppRouter.kLoginView);
+      },
       child: Text(
         'Log In',
         style: TextStyle(

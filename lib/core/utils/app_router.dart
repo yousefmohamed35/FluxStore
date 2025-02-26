e85521b8dd21cb94959963a0c3829d42dafeb3ee
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-26 21:06:15 +0200

    update app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:fluxstore/features/Auth/presentation/view/login_view.dart';
 import 'package:fluxstore/features/Auth/presentation/view/sign_up_view.dart';
 import 'package:fluxstore/features/introduction/presentation/view/intro_view.dart';
 import 'package:fluxstore/features/splash/presentation/view/splash_view.dart';
@@ -6,11 +7,13 @@ import 'package:go_router/go_router.dart';
 abstract class AppRouter {
   static const kIntroView = '/introView';
   static const kSignUpView = '/signUpView';
+  static const kLoginView = '/loginView';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => SplashView()),
       GoRoute(path: kIntroView, builder: (context, state) => IntroView(),),
        GoRoute(path: kSignUpView, builder: (context, state) => SignUpView(),),
+       GoRoute(path: kLoginView,builder: (context, state) => LoginView(),)
     ],
   );
 }

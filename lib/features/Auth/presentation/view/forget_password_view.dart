d76ab6680c070773c1d777eb681970cc8bd192f2
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-26 22:25:41 +0200

    last commit for today

--- lib/features/Auth/presentation/view/forget_password_view.dart
+++ lib/features/Auth/presentation/view/forget_password_view.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fluxstore/core/utils/styles.dart';
 import 'package:fluxstore/core/widgets/custom_back_button.dart';
+import 'package:fluxstore/features/Auth/presentation/view/widgets/sign_up_view_body.dart';
 
 class ForgetPasswordView extends StatelessWidget {
   const ForgetPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ListView(children: [CustomBackButton()]));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            CustomBackButton(),
+            Text("Forget Password ?", style: Styles.textStyle25),
+            SizedBox(height: 20),
+            Text(
+              'Enter email associated with your account and we’ll send and email with intructions to reset your password',
+            ),
+            SizedBox(height: 60),
+            CustomTextField(text: 'Email address'),
+          ],
+        ),
+      ),
+    );
   }
 }
-

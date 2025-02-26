5f751a129fa55e30d12c221ff5b79393c680e6d5
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-26 21:16:28 +0200

    update custom auth button

--- lib/features/Auth/presentation/view/login_view.dart
+++ lib/features/Auth/presentation/view/login_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fluxstore/core/utils/styles.dart';
+import 'package:fluxstore/features/Auth/presentation/view/widgets/custom_auth_button.dart';
+import 'package:fluxstore/features/Auth/presentation/view/widgets/sign_up_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -6,10 +9,28 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 100),
+            Text("Log into", style: Styles.textStyle25),
+            SizedBox(height: 10),
+            Text("your account", style: Styles.textStyle25),
+            SizedBox(height: 50),
+            CustomTextField(text: 'Email address'),
+            SizedBox(height: 20),
+            CustomTextField(text: 'Password'),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Text('Forgot Password?', style: Styles.textStyle12)],
+            ),
+            SizedBox(height: 30),
+            CustomAuthButton(),
+          ],
+        ),
       ),
     );
   }
--- lib/features/Auth/presentation/view/widgets/custom_auth_button.dart
+++ lib/features/Auth/presentation/view/widgets/custom_auth_button.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:fluxstore/core/utils/styles.dart';
 
 class CustomAuthButton extends StatelessWidget {
-  const CustomAuthButton({
-    super.key,
-  });
-
+  const CustomAuthButton({super.key, required this.text, required this.onPressed});
+  final String text;
+  final void Function() onPressed;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: Color(0xff2D201C),
         padding: EdgeInsets.symmetric(horizontal: 48, vertical: 16),
       ),
       child: Text(
-        'Sign up',
+        text,
         style: Styles.textStyle16.copyWith(color: Colors.white),
       ),
     );

6051e7a852c0b48e4a805813875769aeca2123cc
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-27 17:15:08 +0200

    finish custom login form field

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
-
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key, required this.hintText, this.validator});
   final String hintText;
   final String? Function(String?)? validator;
+  final bool? obscureText;
+  final TextEditingController? controller;
+  final Widget? suffix;
+  const CustomTextFormField({
+    super.key,
+    required this.hintText,
+    this.validator,
+    this.obscureText = false,
+    this.controller, this.suffix,
+  });
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      obscureText: obscureText ?? false,
       validator: validator,
-      decoration: InputDecoration(hintText: hintText));
+
+      decoration: InputDecoration(
+        hintText: hintText,
+        suffix: suffix,
+      ),
+      
+    );
   }
 }
--- lib/features/Auth/presentation/view/widgets/custom_login_form_field.dart
+++ lib/features/Auth/presentation/view/widgets/custom_login_form_field.dart
@@ -1,8 +1,9 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:fluxstore/core/functions/text_form_field_validation.dart';
 import 'package:fluxstore/core/widgets/custom_text_form_field.dart';
 import 'package:fluxstore/features/Auth/presentation/view/widgets/custom_auth_button.dart';
 import 'package:fluxstore/features/Auth/presentation/view/widgets/forget_password_button.dart';
-import 'package:fluxstore/features/Auth/presentation/view/widgets/sign_up_view_body.dart';
 
 class CustomLoginFormField extends StatefulWidget {
   const CustomLoginFormField({super.key});
@@ -12,18 +13,58 @@ class CustomLoginFormField extends StatefulWidget {
 }
 
 class _CustomLoginFormFieldState extends State<CustomLoginFormField> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> loginFormKey = GlobalKey();
+  bool obscure = true;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomTextFormField(hintText: 'Email Address',),
-          SizedBox(height: 20),
-          CustomTextField(text: 'Password'),
+    return Form(
+      key: loginFormKey,
+      child: Column(
+        children: [
+          CustomTextFormField(
+            hintText: 'Email Address',
+            controller: emailController,
+            validator: (email) {
+              return validateEmail(email);
+            },
+          ),
+          SizedBox(height: 30),
+          CustomTextFormField(
+            hintText: 'Password',
+            controller: passwordController,
+            obscureText: obscure,
+            suffix: IconButton(
+              onPressed: () {
+                setState(() {
+                  obscure = !obscure;
+                });
+              },
+              icon:
+                  obscure ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
+            ),
+            validator: (password) {
+              return validatePassword(password);
+            },
+          ),
           SizedBox(height: 20),
           ForgetPasswordButton(),
           SizedBox(height: 40),
-          Center(child: CustomAuthButton(text: 'LOG IN', onPressed: () {})),
-      ],
+          Center(
+            child: CustomAuthButton(
+              text: 'LOG IN',
+              onPressed: () {
+                if (loginFormKey.currentState!.validate()) {
+                  log("success");
+                } else {
+                  log("not");
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

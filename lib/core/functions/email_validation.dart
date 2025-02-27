236363556d0c1b17df953f165351d3e3b7262bc4
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-02-27 16:05:35 +0200

    create password validation

--- lib/core/functions/email_validation.dart
+++ lib/core/functions/email_validation.dart
@@ -1,3 +1,4 @@
+// email validation
 String? validateEmail(String? email) {
   final RegExp emailRegex = RegExp(
     r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
@@ -9,4 +10,32 @@ String? validateEmail(String? email) {
     return "Invalid email format";
   }
   return "Valid email";
+}
+
+// password validation 
+String? validatePassword(String? password) {
+  if (password==null || password.isEmpty) {
+    return "Password is required";
+  } else if (password.length < 8) {
+    return "Password must be at least 8 characters long";
+  } else if (!RegExp(r'[A-Z]').hasMatch(password)) {
+    return "Password must contain at least one uppercase letter";
+  } else if (!RegExp(r'[a-z]').hasMatch(password)) {
+    return "Password must contain at least one lowercase letter";
+  } else if (!RegExp(r'\d').hasMatch(password)) {
+    return "Password must contain at least one digit";
+  } else if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(password)) {
+    return "Password must contain at least one special character";
+  }
+  return "Valid password";
+}
+
+// confirm password validation
+String? validateConfirmPassword(String? password, String? confirmPassword) {
+  if (confirmPassword==null || confirmPassword.isEmpty) {
+    return "Confirm password is required";
+  } else if (confirmPassword != password) {
+    return "Passwords do not match";
+  }
+  return "Passwords match";
 }
